83ac3baeb9eccab679e1f7718f3e637466396bbd
JKKIM <jkesonjk@gmail.com>
2023-01-02 23:24:52 +0900

    모듈을 불러와서 declaration file 작성 내용에 따라 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 // const hello = () => "hi";
 
-class Block {
-  constructor(
-    private data: string;
-  ) { }
-  static hello() {
-    return "hi";
-  }
-}
+// class Block {
+//   constructor(
+//     private data: string;
+//   ) { }
+//   static hello() {
+//     return "hi";
+//   }
+// }
+
+
+// node 모듈 패키지에서 import 해서 가져다 쓰듯이 작성
+import { init, exit } from 'myPackage';
+
+init({
+  url: "https://www.test.com"
+})
+
+exit(1)
+
+localStorage.getItem

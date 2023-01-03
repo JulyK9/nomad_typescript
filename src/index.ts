a6942ab8e0c39f04c8fc26845a35233985212a15
JKKIM <jkesonjk@gmail.com>
2023-01-04 03:06:03 +0900

    ts파일에서 js파일 불러오기 설정

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,10 @@
 //   }
 // }
 
-
 // node 모듈 패키지에서 import 해서 가져다 쓰듯이 작성
-import { init, exit } from 'myPackage';
+// import { init, exit } from 'myPackage';
 
-init({
-  url: "https://www.test.com"
-})
+// 파일을 불러오는 방식으로 작성(js 파일을 불러옴)
+import { init, exit } from "./myPackage";
 
-exit(1)
-
-localStorage.getItem
+init();
